645110d729de56fabfc4bfe7807728765d7cae69
Jake Cowton <rockjake28@hotmail.co.uk>
2015-11-13 09:58:23 +0000

    Made infinite loop

--- run.py
+++ run.py
@@ -5,9 +5,10 @@ sites = None
 with open('./opendns-top-domains.txt') as f:
 	sites = f.readlines()
 
-for site in sites:
-	try:
-		requests.get("http://%s" % site.strip('\n'))
-	except:
-		pass
+while True:
+	for site in sites:
+		try:
+			requests.get("http://%s" % site.strip('\n'))
+		except:
+			pass
 
